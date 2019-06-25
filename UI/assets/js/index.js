2649d46fc30bdf6593c36f4e78112bfd60f73445
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-06-25 01:03:13 +0100

    chore(users experience): users experience
    increase the font size of all the modal popup label
    increase the font size of the Join Us anchor tag of the index page
    remove outline on the text input field
    
    [Finishes #166862233]

--- UI/assets/js/index.js
+++ UI/assets/js/index.js
@@ -33,7 +33,7 @@ closeBtn.forEach(close => close.addEventListener('click', e => {
 window.addEventListener('click', e => {
   if(event.target == modal){
     modal.style.display = 'none';
-  } else if(event.target ==modalLogin){
+  } else if(event.target == modalLogin){
     modalLogin.style.display = 'none';
   }
 });
