efdbe44d2797d95711ca2acdabdfb54e1e657ea1
Ropo John Olatujoye <princeolatjohnjoe@yahoo.com>
2019-07-15 09:28:58 -0400

    chore(auto-grader): fix create function

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -72,7 +72,9 @@ class PropertyController {
     try {
       const id = parseInt(req.params.property_id, 10);
       const soldProperty = { status: 'sold' };
-      const property = new PropertyModel({ id, ...soldProperty });
+      const created_on = new Date();
+      const property = new PropertyModel({ id, ...soldProperty, created_on });
+      // console.log('*******', property);
       await property.updateProperty();
       return Response.handleSuccess(200, 'Mark as sold successfully', property.result, res);
     } catch (error) {
